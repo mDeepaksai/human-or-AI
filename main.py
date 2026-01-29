76b8325102394b6e044a803250b56db1a94c1c5e
mDeepaksai <mdeepaksai806@gmail.com>
2026-01-29 21:42:41 +0530

    used post

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from fastapi import FastAPI
-from models import product
+from models import Product
+from typing import List
 
 app = FastAPI()
 
+products: List[Product] = [
+    Product(id=1, name="product_1", quantity=2, price=3),
+    Product(id=2, name="product_2", quantity=2, price=10),
+    Product(id=3, name="product_3", quantity=2, price=20),
+    Product(id=4, name="product_4", quantity=2, price=43),
+]
+
 @app.get("/")
-def geet():
-    return "this is server"
-product=[
-    product(1,"phone",50000,"realme"),
-    product(2,"phone",30000,"opp")
-    ]
-@app.get("/product")
-def get_all_products():
+def hello_world():
+    return {"message": "Hello World This is Deepak Sai"}
+
+@app.get("/products")
+def get_products():
+    return products
+
+@app.get("/products/{id}")
+def get_product(id: int):
+    for p in products:
+        if p.id == id:
+            return p
+    return {"message": f"Product with id {id} not found"}
+
+@app.post("/products")
+def create_product(product: Product):
+    products.append(product)
     return product
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-class product:
-    def __init__(self,id:'int',product_name:'str',price:'int',brand:'str'):
-        self.id=id
-        self.price=price
-        self.brand=brand
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    id: int
+    name: str
+    quantity: int
+    price: float
